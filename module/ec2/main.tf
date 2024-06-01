38484a4cecdb6b5f4bfba946d37072c7792717a6
Darshan Nikam <165551512+darsh-86@users.noreply.github.com>
2024-06-01 12:16:31 +0530

    n

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "this_aws_instance" {
   ami                    = var.this_aws_instance_ami
   instance_type          = var.this_aws_instance_instance_type
   key_name               = var.this_aws_instance_key_name
-  vpc_security_group_ids =  [aws_security_group.this_sg.id]  //[var.this_aws_instance_sg_id]
+  vpc_security_group_ids = [var.aws_security_group.this_sg.id]  //[var.this_aws_instance_sg_id]
   availability_zone      = var.this_aws_instance_availability_zone
   subnet_id = var.this_aws_instance_subnet 
   root_block_device {
