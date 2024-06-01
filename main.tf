b3af1cd5d4119058043deaf973db9f7043f42db0
Darshan Nikam <165551512+darsh-86@users.noreply.github.com>
2024-06-01 14:42:58 +0530

    n

--- main.tf
+++ main.tf
@@ -64,12 +64,12 @@ module "ec2" {
     this_aws_instance_ami = "ami-0b66d4401b55f98c4"
     this_aws_instance_instance_type = "t3.small"
     this_aws_instance_key_name = "parisIAM"
-    this_aws_instance_availability_zone = "eu-west-3b"
+    this_aws_instance_availability_zone = "eu-west-3a"
     this_aws_instance_volume_size = 15
     this_aws_instance_count = 2
     this_aws_instance_user_data_base64 = true
     this_aws_instance_tags = "Terraform_instance"
-    this_aws_instance_subnet = module.vpc.subnet_id
+    this_aws_instance_subnet = module.vpc.subnet.this_subnet_pub.id
     this_sg_vpc_id = module.vpc.vpc_id
     this_aws_instance_sg_id = aws_security_group.this_sg.id
 }
