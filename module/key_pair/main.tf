5f031e8bdc3073e8b1b3ab67f1a432469234b6f8
Darshan Nikam <165551512+darsh-86@users.noreply.github.com>
2024-06-04 17:37:30 +0530

    n

--- module/key_pair/main.tf
+++ module/key_pair/main.tf
@@ -1,6 +1,6 @@
-resource "aws_key_pair" "this" {
-  key_name   = var.key_name
-  public_key = var.public_key
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
 }
 
 resource "local_file" "private_key" {
@@ -11,9 +11,4 @@ resource "local_file" "private_key" {
 resource "local_file" "public_key" {
   content  = tls_private_key.example.public_key_pem
   filename = var.public_key_path
-}
-
-resource "tls_private_key" "example" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
 }
